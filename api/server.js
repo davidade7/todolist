cec289173ada593e30606e2c59232458718c4920
David ADE <david.ade.1989@gmail.com>
2022-04-21 13:42:21 +0200

    feat : CRUD for lists

--- api/server.js
+++ api/server.js
@@ -15,3 +15,53 @@ mongoose.connect('mongodb://127.0.0.1:27017/todosdb', {
   .catch(console.error);
 
 app.listen(3001, () => console.log("PROJECT - Server started on port 3001"));
+
+// ---------------------------
+// Routes for list
+const List = require('./models/List')
+
+// get all lists not archived
+app.get('/lists', async (req, res) => {
+	const lists = await List.find({"isArchived": false});
+	res.json(lists);
+});
+
+// get all archived lists
+app.get('/archived-lists', async (req, res) => {
+	const lists = await List.find({"isArchived": true});
+	res.json(lists);
+});
+
+// create a new list
+app.post('/list/new', (req, res) => {
+	const list = new List({
+		listName: req.body.listName
+	})
+	list.save();
+	res.json(list);
+});
+
+// delete a list
+app.delete('/list/delete/:id', async (req, res) => {
+	const result = await List.findByIdAndDelete(req.params.id);
+	res.json({result});
+});
+
+// Archive or not a list
+app.get('/list/archive/:id', async (req, res) => {
+	const list = await List.findById(req.params.id);
+	list.isArchived = !list.isArchived;
+	list.save();
+
+	res.json(list);
+})
+
+// update list name
+app.put('/list/update/:id', async (req, res) => {
+	const list = await List.findById(req.params.id);
+	list.listName = req.body.listName;
+	list.save();
+
+	res.json(list);
+});
+
