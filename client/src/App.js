5a3f72af5fb870e7c21a71baafdf6dd4892e574b
David ADE <david.ade.1989@gmail.com>
2022-04-21 15:14:45 +0200

    feat: adding header and its style

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
+// import of external modules
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
+// images and icones
 import './App.css';
 
 function App() {
+  const [ongoing, setOngoing] = useState(true);
+  
   return (
     <div className="App">
+      <BrowserRouter>
+        <header>
+          <div className="header-content">
+            <div className="header-title">
+              <h1>Mes Listes</h1>
+            </div>
+            <nav className="header-nav">
+              <ul>
+                <li>
+                  <NavLink
+                    to="ongoing"
+                    className={({ isActive }) =>
+                      isActive ? 'active' : undefined
+                    }
+                    onClick={() => setOngoing(true)}
+                  >
+                    En cours
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="completed"
+                    className={({ isActive }) =>
+                      isActive ? 'active' : undefined
+                    }
+                    onClick={() => setOngoing(false)}
+                  >
+                    Terminées
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>          
+        </header>
+
+      </BrowserRouter>
       
     </div>
   );
